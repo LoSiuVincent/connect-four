222a8cc4ee2035fe940dc9378f6788c67891366d
Vincent Lo <lovincent536@gmail.com>
2024-01-28 19:59:28 +0800

    Added a drop coin functional test

--- tests/test_functionals.py
+++ tests/test_functionals.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+import time
 import pytest
 
 
@@ -39,3 +40,47 @@ def test_layout(browser):
     canvas_center_y = canvas.location['y'] + canvas.size['height'] / 2
     assert canvas_center_x == 800
     assert abs(canvas_center_y - 500) < 100
+
+
+def test_drop_coins_to_board(browser):
+    # John goes to the website
+    browser.get('http://localhost:8000')
+
+    # John sees the canvas for the game
+    canvas = browser.find_element(By.TAG_NAME, 'canvas')
+    assert canvas is not None
+
+    def click_column(column_index):
+        board_x = browser.execute_script('return game.getBoardX();')
+        board_y = browser.execute_script('return game.getBoardY();')
+        cell_width = browser.execute_script('return game.getCellWidth();')
+        
+        click_x = board_x + column_index * cell_width + 0.5 * cell_width
+        click_y = board_y - 10
+        
+        action = webdriver.common.action_chains.ActionChains(browser)
+        action.move_to_element_with_offset(canvas, click_x, click_y)
+        action.click()
+        action.perform()
+
+        time.sleep(1)
+
+    # He clicks on the first column
+    click_column(0)
+
+    first_column_bottom_cell_state = browser.execute_script("return game.getCellState(0, 0);")
+    assert first_column_bottom_cell_state == 'player'
+
+    # He clicks another column
+    click_column(3)
+
+    # Check the game state to ensure a coin has been added to the fourth column
+    fourth_column_bottom_cell_state = browser.execute_script("return game.getCellState(3, 0);")
+    assert fourth_column_bottom_cell_state == 'player'
+
+    # He tries to drop a coin at the first column again
+    click_column(0)
+
+    # Check the game state to ensure another coin has been stacked in the first column
+    first_column_second_last_bottom_cell_state = browser.execute_script("return game.getCellState(0, 1);")
+    assert first_column_second_last_bottom_cell_state == 'player'
