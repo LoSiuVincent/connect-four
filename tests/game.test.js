93d176748caab5b50cbc075292daff1473684f2b
Vincent Lo <lovincent536@gmail.com>
2024-01-28 21:38:33 +0800

    Changed from toEqual to toBe in unit tests

--- tests/game.test.js
+++ tests/game.test.js
@@ -7,17 +7,17 @@ test("create a Game object without problem", () => {
 test("get board x from the Game object", () => {
 	const game = new Game(10);
 
-	expect(game.getBoardX()).toEqual(10);
+	expect(game.getBoardX()).toBe(10);
 })
 
 test("get board y from the Game object", () => {
 	const game = new Game(10, 20);
 
-	expect(game.getBoardY()).toEqual(20);
+	expect(game.getBoardY()).toBe(20);
 })
 
 test("get cell width from the Game object", () => {
 	const game = new Game(10, 20, 100);
 
-	expect(game.getCellWidth()).toEqual(100);
+	expect(game.getCellWidth()).toBe(100);
 })
