70537271548ccf71c007cef4816ef3d82581ffc8
Vincent Lo <lovincent536@gmail.com>
2024-01-28 22:55:53 +0800

    Refactored unit test to not use board x y

--- tests/game.test.js
+++ tests/game.test.js
@@ -4,20 +4,8 @@ test("create a Game object without problem", () => {
 	const game = new Game();
 });
 
-test("get board x from the Game object", () => {
-	const game = new Game(10);
+test("get cell length from the Game object", () => {
+	const game = new Game(100);
 
-	expect(game.getBoardX()).toBe(10);
-})
-
-test("get board y from the Game object", () => {
-	const game = new Game(10, 20);
-
-	expect(game.getBoardY()).toBe(20);
-})
-
-test("get cell width from the Game object", () => {
-	const game = new Game(10, 20, 100);
-
-	expect(game.getCellWidth()).toBe(100);
-})
+	expect(game.getCellLength()).toBe(100);
+});
