7def457b5201b30a426de9c7e6749c8b3828b746
Vincent Lo <lovincent536@gmail.com>
2024-01-30 00:25:15 +0800

    Now the canvas can draw a board

--- static/sketch.js
+++ static/sketch.js
@@ -1,4 +1,5 @@
 import { Game } from "./game.js";
+import { p5View } from "./view.js";
 
 const CELL_SIZE = 100;
 const game = new Game(CELL_SIZE);
@@ -7,8 +8,10 @@ window.game = game;
 new p5((p5) => {
 	p5.setup = () => {
 		p5.createCanvas(game.getCanvasWidth(), game.getCanvasHeight());
+		window.view = new p5View(game, p5);
 	};
 
 	p5.draw = () => {
+		window.view.draw();
 	};
 });
--- static/view.js
+++ static/view.js
@@ -1,5 +1,28 @@
 export class p5View {
-	constructor() {}
+	constructor(game, p5) {
+		this.game = game;
+		this.p5 = p5;
+	}
 
-	drawBoard() {}
+	draw() {
+		this._drawBoard();
+	}
+
+	_drawCell(row, col) {
+		const cellLength = this.game.getCellLength();
+		const cornerX = col * cellLength;
+		const cornerY = 2 * cellLength + row * cellLength;
+
+		this.p5.stroke(0, 0, 0);
+		this.p5.strokeWeight(4);
+		this.p5.square(cornerX, cornerY, cellLength);
+	}
+
+	_drawBoard() {
+		for (let i = 0; i < 6; i++) {
+			for (let j = 0; j < 7; j++) {
+				this._drawCell(i, j);
+			}
+		}
+	}
 }
