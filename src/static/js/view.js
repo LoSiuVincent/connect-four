460203560f7ddb56d8ef0206cfcba5e83e56ea27
Vincent Lo <lovincent536@gmail.com>
2024-01-30 20:56:55 +0800

    Now a coin will shop up when the Game object update

--- src/static/js/view.js
+++ src/static/js/view.js
@@ -24,17 +24,33 @@ export class p5View {
 	_drawCell(row, col) {
 		const cellLength = this.cellLength;
 		const cornerX = col * cellLength;
-		const cornerY = 2 * cellLength + row * cellLength;
+		const cornerY = 2 * cellLength + (5 - row) * cellLength;
 
+		this.p5.fill(255, 255, 255);
 		this.p5.stroke(0, 0, 0);
 		this.p5.strokeWeight(4);
 		this.p5.square(cornerX, cornerY, cellLength);
+	}
+
+	_drawCoin(row, col) {
+		const diameter = this.cellLength * 0.75;
+		const cornerX = col * this.cellLength;
+		const cornerY = 2 * this.cellLength + (5 - row) * this.cellLength;
+		const centerX = cornerX + this.cellLength / 2;
+		const centerY = cornerY + this.cellLength / 2;
+
+		this.p5.fill(255, 0, 0);
+		this.p5.noStroke();
+		this.p5.circle(centerX, centerY, diameter);
 	}
 
 	_drawBoard() {
 		for (let i = 0; i < 6; i++) {
 			for (let j = 0; j < 7; j++) {
 				this._drawCell(i, j);
+				if (this.game.getCellState(i, j) === "player") {
+					this._drawCoin(i, j);
+				}
 			}
 		}
 	}
---
+++ tests/test_integrations.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+import time
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format=r'[%(levelname)s] %(message)s')
+
+BASELINE_IMG_DIR = Path(__file__).resolve().parent / 'baseline'
+CURRENT_IMG_DIR = Path(__file__).resolve().parent / 'current'
+
+@pytest.fixture
+def browser():
+    driver = webdriver.Firefox()
+    driver.get('http://localhost:8000')
+    yield driver
+    driver.quit()
+
+@pytest.mark.visual
+def test_coins_show_up_when_the_game_drops_the_coins(browser, image_diff):
+    canvas = browser.find_element(By.TAG_NAME, 'canvas')
+    time.sleep(0.5)
+
+    browser.execute_script('game.dropCoin(0);')
+    time.sleep(1)
+
+    baseline_image_path = str(BASELINE_IMG_DIR / 'first_coin.png')
+    current_image_path = str(CURRENT_IMG_DIR / 'first_coin.png')
+    if int(os.environ.get('UPDATE_BASELINE', 0)) == 1:
+        canvas.screenshot(baseline_image_path)
+        logging.info(f'Captured image to {baseline_image_path}')
+    else:
+        canvas.screenshot(current_image_path)
+        assert image_diff(baseline_image_path, current_image_path)
