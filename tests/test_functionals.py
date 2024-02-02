6d3ffb0d120d8bf9e8f6c9a09c08a600cf0257fe
Vincent Lo <lovincent536@gmail.com>
2024-02-02 20:44:11 +0800

    Extracted helper method out of the functional test

--- tests/test_functionals.py
+++ tests/test_functionals.py
@@ -11,6 +11,32 @@ from tests.conftest import BROWSER_WIDTH
 from tests.visreg import web_element_regression
 
 
+def click_column(browser, canvas, column_index):
+    cell_width = browser.execute_script('return view.getCellLength();')
+
+    click_x = column_index * cell_width + 0.5 * cell_width - canvas.size['width'] / 2
+    click_y = 100 - canvas.size['height'] / 2
+
+    action = webdriver.common.action_chains.ActionChains(browser)
+    action.move_to_element_with_offset(canvas, click_x, click_y)
+    action.click()
+    action.perform()
+
+    time.sleep(1)
+
+
+def wait_until_text_appear(browser, text: str):
+    WebDriverWait(browser, 5).until(
+        EC.text_to_be_present_in_element((By.ID, 'id-game-state-text'), text)
+    )
+
+
+def wait_until_text_disappear(browser, text: str):
+    WebDriverWait(browser, 5).until_not(
+        EC.text_to_be_present_in_element((By.ID, 'id-game-state-text'), text)
+    )
+
+
 def test_canvas_exists(browser):
     # He sees a canvas where he can play the game
     try:
@@ -49,70 +75,47 @@ def test_play_with_the_computer(browser):
     canvas = browser.find_element(By.TAG_NAME, 'canvas')
     assert canvas is not None
 
-    def click_column(column_index):
-        cell_width = browser.execute_script('return view.getCellLength();')
-
-        click_x = column_index * cell_width + 0.5 * cell_width - canvas.size['width'] / 2
-        click_y = 100 - canvas.size['height'] / 2
-
-        action = webdriver.common.action_chains.ActionChains(browser)
-        action.move_to_element_with_offset(canvas, click_x, click_y)
-        action.click()
-        action.perform()
-
-        time.sleep(1)
-
-    def wait_until_text_appear(text: str):
-        WebDriverWait(browser, 5).until(
-            EC.text_to_be_present_in_element((By.ID, 'id-game-state-text'), text)
-        )
-
-    def wait_until_text_disappear(text: str):
-        WebDriverWait(browser, 5).until_not(
-            EC.text_to_be_present_in_element((By.ID, 'id-game-state-text'), text)
-        )
-
     state_text = browser.find_element(By.ID, 'id-game-state-text')
 
     # He sees "Your turn" and he clicks the first column
-    wait_until_text_appear('Your turn')
-    click_column(0)
+    wait_until_text_appear(browser, 'Your turn')
+    click_column(browser, canvas, 0)
 
     # He sees a coin has appear
     web_element_regression(canvas, 'player_first_move', wait_time_before_capture=0)
 
     # He sees the text changed and became "Thinking ..." afterward
-    wait_until_text_disappear('Your turn')
+    wait_until_text_disappear(browser, 'Your turn')
     assert state_text.text == 'Thinking ...'
 
     # He sees "Your turn" again after a while and the computer has made a move
-    wait_until_text_appear('Your turn')
+    wait_until_text_appear(browser, 'Your turn')
     web_element_regression(canvas, 'computer_first_move', wait_time_before_capture=0)
 
     # He clicks another column
-    click_column(3)
+    click_column(browser, canvas, 3)
 
     # He sees a coin has appear
     web_element_regression(canvas, 'player_second_move', wait_time_before_capture=0)
 
     # He sees the text changed and became "Thinking ..." afterward
-    wait_until_text_disappear('Your turn')
+    wait_until_text_disappear(browser, 'Your turn')
     assert state_text.text == 'Thinking ...'
 
     # He sees "Your turn" again after a while and the computer has made a move
-    wait_until_text_appear('Your turn')
+    wait_until_text_appear(browser, 'Your turn')
     web_element_regression(canvas, 'computer_second_move', wait_time_before_capture=0)
 
     # He clicks the first column again
-    click_column(0)
+    click_column(browser, canvas, 0)
 
     # He sees the coin is stacked above the first coin
     web_element_regression(canvas, 'player_third_move', wait_time_before_capture=0)
 
     # He sees the text changed and became "Thinking ..." afterward
-    wait_until_text_disappear('Your turn')
+    wait_until_text_disappear(browser, 'Your turn')
     assert state_text.text == 'Thinking ...'
 
     # He sees "Your turn" again after a while and the computer has made a move
-    wait_until_text_appear('Your turn')
+    wait_until_text_appear(browser, 'Your turn')
     web_element_regression(canvas, 'computer_third_move', wait_time_before_capture=0)
