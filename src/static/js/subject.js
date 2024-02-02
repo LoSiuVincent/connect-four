6c278b6c4179aadb6f27bc949827d9832b400584
Vincent Lo <lovincent536@gmail.com>
2024-02-02 18:41:21 +0800

    Added Subject Class

---
+++ src/static/js/subject.js
@@ -0,0 +1,25 @@
+export class Subject {
+    constructor() {
+        this.callbacks = [];
+    }
+
+    addListener(event, listener) {
+        this.callbacks.push([event, listener]);
+    }
+
+    notify(event, data) {
+
+        const promises = [];
+
+        this.callbacks.forEach((eventCallbackTuple) => {
+            const listenEvent = eventCallbackTuple[0];
+            const listener = eventCallbackTuple[1];
+            if (event === listenEvent) {
+                const updatePromise = listener.update(data);
+                promises.push(updatePromise);
+            }
+        });
+
+        return Promise.all(promises);
+    }
+}
---
+++ tests/frontend/unit/subject.test.js
@@ -0,0 +1,20 @@
+import { Subject } from "subject.js";
+import { jest, expect, describe, test } from "@jest/globals";
+
+test("subject should notify to the observers", async () => {
+    const subject = new Subject();
+
+    const listenerOne = { update: jest.fn() };
+    const listenerTwo = { update: jest.fn() };
+    const listenerThree = { update: jest.fn() };
+    subject.addListener("eventOne", listenerOne);
+    subject.addListener("eventTwo", listenerTwo);
+    subject.addListener("eventThree", listenerThree);
+
+    await subject.notify("eventOne", { a: 1, b: 2 });
+    await subject.notify("eventTwo", { a: 10, b: 20 });
+
+    expect(listenerOne.update).toHaveBeenCalledWith({ a: 1, b: 2 });
+    expect(listenerTwo.update).toHaveBeenCalledWith({ a: 10, b: 20 });
+    expect(listenerThree.update).not.toHaveBeenCalled();
+});
