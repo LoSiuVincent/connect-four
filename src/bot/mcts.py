29b41c3086f51e2a9ca93ff97f3f7367d0ee6041
Vincent Lo <lovincent536@gmail.com>
2024-02-04 22:43:09 +0800

    Added select in MCTS

--- src/bot/mcts.py
+++ src/bot/mcts.py
@@ -1,7 +1,13 @@
+import math
+
+
 class Node:
-    def __init__(self):
+    def __init__(self, state):
         self._parent = self
         self._children = []
+        self._state = state
+        self.n = 0
+        self.v = 0
 
     def is_leaf(self):
         return len(self._children) == 0
@@ -11,5 +17,37 @@ class Node:
             child._parent = self
         self._children.extend(children)
 
+    def get_children(self):
+        return self._children
+
     def get_parent(self):
         return self._parent
+
+    def __repr__(self):
+        return f'Node(n={self.n}, v={self.v})'
+
+
+class MCTS:
+    def __init__(self, game_state, C=1):
+        self._root = Node(game_state)
+        self._C = C
+
+    def select(self):
+        current = self._root
+        if current.is_leaf():
+            return current
+        else:
+            return self._get_best_child(current)
+
+    def _get_best_child(self, parent):
+        UCBs = [self._calculate_UCB(child) for child in parent.get_children()]
+        max_UCB = max(UCBs)
+        for idx, value in enumerate(UCBs):
+            if value == max_UCB:
+                return parent.get_children()[idx]
+
+    def _calculate_UCB(self, node):
+        if node.n == 0:
+            return math.inf
+        else:
+            return node.v / node.n + self._C * math.sqrt(math.log(node.get_parent().n) / node.n)
---
+++ tests/backend/unit/test_mcts.py
@@ -0,0 +1,48 @@
+from unittest.mock import Mock
+
+import pytest
+
+from src.bot.mcts import MCTS, Node
+
+
+def test_create_root_node():
+    game_state = Mock()
+    mcts = MCTS(game_state)
+
+    assert mcts._root._state == game_state
+    assert mcts._root.n == 0
+    assert mcts._root.v == 0
+
+
+def test_select_root_node():
+    game_state = Mock()
+    mcts = MCTS(game_state)
+
+    assert mcts.select() == mcts._root
+
+
+@pytest.mark.parametrize(
+    'children_n_v,select_child,C',
+    [
+        ([(1, 1), (2, 1)], 0, 1.4),
+        ([(10, 5), (5, 5)], 1, 1.4),
+        ([(1, 1), (5, 10)], 1, 0),
+        ([(10, 50), (10, 50), (1, 5)], 2, 5.0),
+        ([(0, 0), (0, 0)], 0, 1.4),  # When all nodes have the same n and v, select the first
+    ],
+)
+def test_select_child_node_with_higher_UCB(children_n_v, select_child, C):
+    some_game_state = Mock()
+    root = Node(some_game_state)
+    children = []
+    for n, v in children_n_v:
+        child = Node(some_game_state)
+        child.n = n
+        child.v = v
+        children.append(child)
+    root.add_children(children)
+    root.n = len(children)
+    mcts = MCTS(some_game_state, C=C)
+    mcts._root = root
+
+    assert mcts.select() == children[select_child]
